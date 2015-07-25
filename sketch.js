562ecfb93b7b23e4f94f1f9a5ca47820d6fe54a2
scottusv3 <scottus@gmail.com>
2015-07-25 13:49:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,17 +2,16 @@ function setup() {
   // uncomment this line to make the canvas the full size of the window
   createCanvas(windowWidth, windowHeight);
   background(12);
+  x=12;y=12;  r=255,g=0,b=0;
 }
 
 function draw() {
   // draw stuff here
 
-  fill(255,0,0);
+   fill(r,g,b);
+   rect(x, y, 50, 50);
 
 
-  stroker (255,0,0)
-
-  rect(10,10,100,100)
-
+x = x+ 5; y = y + 5;  r=r-5;
 
 }
