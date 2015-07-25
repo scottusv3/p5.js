a3391a9da18fef212536e7a04816970c28e80c8c
scottusv3 <scottus@gmail.com>
2015-07-25 13:45:24 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  createCanvas(windowWidth, windowHeight);
+  background(12);
+}
+
+function draw() {
+  // draw stuff here
+
+  fill(255,0,0);
+
+
+  stroker (255,0,0)
+
+  rect(10,10,100,100)
+
+
+}
